e20bc217564aba9dedec6f5c8a15d3eafb07433a
yilmaz673 <108520190+yilmaz673@users.noreply.github.com>
2023-04-13 22:52:08 +0300

    Add files via upload

---
+++ kralice0.3.py
@@ -0,0 +1,33 @@
+import telegram
+import telegram.ext
+import datetime
+import pytz
+
+TOKEN = '6245058500:AAEudc8D7yt1fvYDrKGscscJ_6JMsV2pJR0'
+iftar_times = [    datetime.datetime(2023, 4, 12, 20, 5, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 13, 20, 31, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 14, 20, 33, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 15, 20, 34, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 16, 20, 36, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 17, 20, 38, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 18, 20, 39, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 19, 20, 41, tzinfo=pytz.timezone('Europe/Berlin')),    datetime.datetime(2023, 4, 20, 20, 42, tzinfo=pytz.timezone('Europe/Berlin'))]
+
+
+def get_iftar_time():
+    now = datetime.datetime.now(tz=pytz.timezone('Europe/Berlin')) + datetime.timedelta(hours=1, minutes=7)
+    for iftar_time in iftar_times:
+        if iftar_time > now:
+            time_left = iftar_time - now
+            time_left = time_left 
+            day = iftar_time.strftime("%A")
+            return f"Kraliçemin karnının doymasına {(time_left.seconds // 3600)} saat, {(time_left.seconds // 60) % 60} dakika kaldı."
+    return "Kraliçemin karnı doydu :)"
+
+def iftar(update, context):
+    message = get_iftar_time()
+    context.bot.send_message(chat_id=update.message.chat_id, text=message, reply_to_message_id=update.message.message_id)
+
+def main():
+    updater = telegram.ext.Updater(TOKEN, use_context=True)
+
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler('iftar', iftar))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
